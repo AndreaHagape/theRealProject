719d665a2b283945df45030638366f8a6b651bb1
AndreaH <805133@apps.district196.org>
2020-01-30 07:56:51 -0600

    1/30/20

--- theRealProject/ContentView.swift
+++ theRealProject/ContentView.swift
@@ -10,7 +10,10 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, World!")
+        ZStack {
+            Color.green
+                .edgesIgnoringSafeArea(.all)
+        }
     }
 }
 
@@ -19,3 +22,4 @@ struct ContentView_Previews: PreviewProvider {
         ContentView()
     }
 }
+
